bc4e846e8827a13c0507c4560e2a75b00cf07f29
jafemo <montajavi@gmail.com>
2016-10-11 13:30:37 +0200

    Fase 2

---
+++ lib/ia.php
@@ -0,0 +1,33 @@
+<?php
+//////INICIAMOS LA CLASE//////
+class ia{
+
+	private $columna= '1';
+	private $fila= '1';
+
+	/*public function __construct($columna,$fila){
+		$this->columna=$columna;
+		$this->fila=$fila;
+	}
+
+/*public function randomPos($columna, $fila){
+	$this->columna=rand(0,3);
+	$this->fila=rand(0,3);
+*/
+//////ESTABLECEMOS LOS GETERS//////
+	public function getColumna(){
+		return $this->columna;
+	}
+
+	public function getFila(){
+		return $this->fila;
+	}
+//////Y AHORA LOS SETTERS CON DOS VARIABLES DIFERENTES//////
+	public function setColumna($col){
+		$this->columna= $col;
+	}
+	public function setFila($fil){
+		$this->fila = $fil;
+	}
+}
+?>
